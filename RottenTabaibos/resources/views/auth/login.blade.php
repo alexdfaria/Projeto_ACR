779cab013a7e18bac31a2a06533a4eb738e6d55a
HannibalLecter <brunor_gon_rocha@hotmail.com>
2019-11-04 15:15:55 +0000

    third

--- RottenTabaibos/resources/views/auth/login.blade.php
+++ RottenTabaibos/resources/views/auth/login.blade.php
@@ -1,6 +1,84 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RottenTabaibos</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.11.1/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo-container">
+        </div>
+
+        <div class="topnav">
+            <a class="principal" href="/home">Home</a>
+            <a href="{{ route('register') }}">Register</a>
+        </div>
+
+    </header>
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf 
+        <div class="avatar-container"> 
+            
+            <img src= "/images/logo/1.png" alt="Avatar" class="avatar"> 
+        </div> 
+  
+        <div class="login-container"> 
+            <label for="email"><b>{{ __('E-Mail Address') }}</b></label> 
+            <input type="text" placeholder="Inserir Nome do Utilizador / email" name="email" required> 
+
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+  
+            <label><b>Palavra Passe</b></label> 
+            <input type="password" placeholder="Inserir Palavra Passe" name="password" required> 
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+
+            <button type="submit" class="loginbtn">
+                {{ __('Login') }}
+            </button>
+
+            <input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                <label class="form-check-label" for="remember">
+                    {{ __('Memorizar Palavra Passe') }}
+                </label>
+
+            @if (Route::has('password.request'))
+                <span class="psw">Esqueceu a 
+                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                        {{ __('Palavra Passe') }}
+                    </a>
+                </span>
+            @endif
+            <br>
+            <br>
+
+            <span class="reg">Ainda não está registado? <a href="{{ route('register') }}">Registar</a></span>
+            
+        </div> 
+    </form> 
+</body>
+
+</html>
+
+<!--
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -16,12 +94,6 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
                             </div>
                         </div>
 
@@ -38,36 +110,10 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+-->
--- RottenTabaibos/resources/views/auth/register.blade.php
+++ RottenTabaibos/resources/views/auth/register.blade.php
@@ -1,6 +1,85 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RottenTabaibos</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.11.1/css/all.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="logo-container">
+            <img src="/images/logo/1.png" alt="logo" height="38">
+        </div>
+        <div class="topnav">
+                <a class="principal" href="/home">Home</a>
+                <a href="{{ route('login') }}">Login</a>
+        </div>
+
+    </header>
+
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            
+            <div class="container">
+                <h1 style="text-align: center;">Registo</h1>
+                <p>Por favor insira os dados de modo a criar uma conta </p>
+                <hr>                
+               
+                <form>
+                    <label for="utilizador_tipo"><b>Tipo de Utilizador</b></label> <br>
+                        <div class="utilizadores">
+                            <label class="radio-inline">
+                              <input type="radio" name="utilizador">Comum
+                            </label>
+                            <label class="radio-inline">
+                              <input type="radio" name="utilizador">Critico Profissional
+                            </label>
+                        </div>
+                </form>   
+                <hr>
+                
+
+                <label for="primmeiro_nome"><b>Primeiro Nome</b></label>
+                <input type="text" placeholder="Inserir Primeiro Nome" name="primeiro_nome" required>
+
+                <label for="ultimo_nome"><b>Ultimo Nome</b></label>
+                <input type="text" placeholder="Inserir Ultimo Nome" name="ultimo_nome" required>
+
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Inserir Email" name="email" required>
+
+<!--Mostrar só se for selecionado o utilizador Critico-->
+
+                <label for="jornal"><b>Jornal/Revista/Entidade Crítica</b></label>
+                <input type="text" placeholder="Inserir Empresa que representa" name="jornal" required>
+<!------------------------------------------------------>
+                <label for="password"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
+
+                <label for="password_confirmation"><b>Repetir a Password</b></label>
+                <input type="password" placeholder="Repetir Password" name="password_confirmation" required>
+                
+                
+                <hr>
+
+                <p>Ao criar a conta, concorda com os nossos <a href="#">Termos e Privacidade</a>.</p>
+                <button type="submit" class="registerbtn">{{ __('Register') }}</button>
+            </div>
+
+            <div class="container signin">
+                <p>Already have an account? <a href="{{ route('login') }}">Sign in</a>.</p>
+            </div>
+        </form>
+</body>
+
+</html>
+<!--
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -74,4 +153,4 @@
         </div>
     </div>
 </div>
-@endsection
+-->
